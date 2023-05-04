5f5a8e15f2167911b56201982f5a0466b3773e59
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-05-04 16:52:08 +0200

    fix code

--- src/org/lessons/java/CibiPreferiti.java
+++ src/org/lessons/java/CibiPreferiti.java
@@ -2,15 +2,26 @@ package org.lessons.java;
 
 public class CibiPreferiti {
 	public static void main(String[] args) {
-		String[] cibiPreferiti = {"Pizza", "Sushi", "Risotto allo zafferano", "Spaghetti allo scoglio", "Spaghetti alle vongole", "Risotto agli asparagi", "Zuppa di cozze", "Fritto misto"};
-		
+		String[] cibiPreferiti = {
+			"Pizza",
+			"Sushi",
+			"Risotto allo zafferano",
+			"Spaghetti allo scoglio",
+			"Spaghetti alle vongole",
+			"Risotto agli asparagi",
+			"Zuppa di cozze",
+			"Fritto misto"
+		};
 		System.out.println("La lunghezza dell'array é: " + cibiPreferiti.length);
 		System.out.println("Il mio cibo preferito è: " + cibiPreferiti[0]);
 		System.out.println("Il mio cibo meno preferito è: " + cibiPreferiti[cibiPreferiti.length - 1]);
+		int medianIndex = cibiPreferiti.length / 2;
 		if (cibiPreferiti.length % 2 == 0) {
-			System.out.println("A metà c'è: " + cibiPreferiti[cibiPreferiti.length / 2 - 1]);
+			String medianFoodLow = cibiPreferiti[medianIndex];
+			String medianFoodhigh = cibiPreferiti[medianIndex - 1];
+			System.out.println("A metà ci sono: " + medianFoodLow + " e " + medianFoodhigh);
 		} else {
-			System.out.println("A metà c'è: " + cibiPreferiti[(cibiPreferiti.length - 1) / 2]);
+			System.out.println("A metà c'è: " + cibiPreferiti[medianIndex]);
 		}
 	}
 }
